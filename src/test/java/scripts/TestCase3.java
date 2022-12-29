4bb7b34bff095eac0323c7384540b1504ef1ed58
Praveen B Ronad <Praveen B Ronad@LAPTOP-LCLJAJ2G>
2022-12-29 09:57:09 +0530

    Demo Scripts for git push example

---
+++ src/test/java/scripts/TestCase3.java
@@ -0,0 +1,14 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestCase3 {
+@Test
+public void demo()
+{
+	Reporter.log("Hello API ",true);
+
+
+}
+}
---
+++ src/test/java/scripts/TestCase4.java
@@ -0,0 +1,14 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestCase4 {
+@Test
+public void demo()
+{
+	Reporter.log("Hello API ",true);
+
+
+}
+}
